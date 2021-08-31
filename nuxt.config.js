b9e0678102eef078d9e25702e4d449a7662d3e97
lockdef <locktki@icloud.com>
2021-09-01 00:05:49 +0900

    fix: vue-bootstrapをbuildModuleから削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,6 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
   ],
