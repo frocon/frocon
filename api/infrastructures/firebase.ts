59dd31d6e7add21180151f0684585891fc4cdf0e
lockdef <locktki@icloud.com>
2021-09-02 15:41:21 +0900

    doc: JSDocを追加

--- api/infrastructures/firebase.ts
+++ api/infrastructures/firebase.ts
@@ -4,9 +4,12 @@ import serviceAccount from './serviceAccount.json'
 
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
 
+/**
+ * FirebaseのidTokenを検証する。検証成功すればuidを返す。
+ */
 const verifyIdToken = async (req: Express.Request) => {
   const idToken = req.header('Authorization')
-  if (!idToken) return new Error('Authorization does not exist in Header.')
+  if (!idToken) return ''
   const { uid } = await admin.auth().verifyIdToken(idToken)
   return uid
 }
