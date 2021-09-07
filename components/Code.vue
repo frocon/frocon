963af19a09b9581c2b304e96fc92a26d7f3edea1
kantarow <kanta7306@gmail.com>
2021-09-08 02:40:30 +0900

    feat: シンタックスハイライト

---
+++ components/Code.vue
@@ -0,0 +1,25 @@
+<template>
+  <pre class="p-4"><code v-html="highlight(code)" /></pre>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import highlightjs from 'highlightjs'
+
+export default Vue.extend({
+  name: 'Code',
+  props: {
+    code: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    highlight(code: string) {
+      const html = highlightjs.highlightAuto(code, ['javascript']).value
+      console.log(html)
+      return html
+    },
+  },
+})
+</script>
