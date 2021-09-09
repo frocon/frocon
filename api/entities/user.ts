e2a61d0a2464c0392f13244b9dd5602a2c21c4e9
FlexiblePrintedCircuits <yugehayata.dev@gmail.com>
2021-09-10 04:21:25 +0900

    fix: nameのnullを許可

--- api/entities/user.ts
+++ api/entities/user.ts
@@ -1,9 +1,9 @@
 export class User {
   id: string
-  name: string
+  name: string | null
   email: string
 
-  constructor(id: string, name: string, email: string) {
+  constructor(id: string, name: string | null, email: string) {
     this.id = id
     this.name = name
     this.email = email
