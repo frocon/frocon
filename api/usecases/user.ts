28fa5c595b064f2937a3246a7323b6ec5c61fb30
FlexiblePrintedCircuits <yugehayata.dev@gmail.com>
2021-09-10 02:11:30 +0900

    fix: createNewUserを呼び出すようにした

--- api/usecases/user.ts
+++ api/usecases/user.ts
@@ -1,22 +1,19 @@
 import UserRepository from '../repositories/user'
-import ProjectRepository from '../repositories/project'
 
 const getUserUseCase = async (id: string) => {
   const projectRepository = new UserRepository()
   return await projectRepository.findById(id)
 }
 
-const createUserUseCase = async (project: { name: string }) => {
+const createUserUseCase = async (user: { name: string, email: string, avatar: string | null, firebaseIdToken: string }) => {
   const userRepository = new UserRepository()
-  const projectRepository = new ProjectRepository()
 
-  const loginUser = await userRepository.findLoginUser()
-  if (loginUser !== null) {
-    return await projectRepository.createWithInitialMember(
-      project.name,
-      loginUser.id
-    )
-  }
+  return await userRepository.createNewUser(
+    user.name,
+    user.email,
+    user.avatar,
+    user.firebaseIdToken
+  )
 }
 
 export { getUserUseCase, createUserUseCase }
