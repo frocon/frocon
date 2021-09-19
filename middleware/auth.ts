c2a9072960b3493aad8d52a9e6dcc9e23971a21e
lockdef <locktki@icloud.com>
2021-09-20 01:34:32 +0900

    feat: @nuxtjs/firebaseの実装に変更

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,14 +1,8 @@
 import { Context, Middleware } from '@nuxt/types'
-import firebase from 'firebase'
 
-const authMiddleware: Middleware = ({ app, route, redirect }: Context) => {
-  const unsubscribe = app.$fire.auth.onAuthStateChanged(
-    (user: firebase.User) => {
-      if (!user) return redirect('/login')
-      if (route.path === '/login') return redirect('/')
-    }
-  )
-  unsubscribe()
+const authMiddleware: Middleware = ({ store, route, redirect }: Context) => {
+  if (!store.getters.isLoggedIn) return redirect('/login')
+  if (route.path === '/login') return redirect('/')
 }
 
 export default authMiddleware
