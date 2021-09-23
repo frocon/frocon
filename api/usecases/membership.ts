91b379b4b45563458be8c61ed329a5599188a9e2
lockdef <locktki@icloud.com>
2021-09-24 00:00:35 +0900

    feat: emailからuidを特定

--- api/usecases/membership.ts
+++ api/usecases/membership.ts
@@ -1,9 +1,13 @@
 import MemberCollectionRepository from '../repositories/membership'
+import UserRepository from '../repositories/user'
 
 export const createMembershipUseCase = async (
   projectId: string,
-  userId: string
+  email: string
 ) => {
   const memberCollectionRepository = new MemberCollectionRepository()
+  const userRepository = new UserRepository()
+  const user = await userRepository.findByEmail(email)
+  const userId = user.id
   return await memberCollectionRepository.createMembership(projectId, userId)
 }
