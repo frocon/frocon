2c72f175b02e5026de229281a854e8966d0de874
FlexiblePrintedCircuits <yugehayata.dev@gmail.com>
2021-09-25 22:41:04 +0900

    fix: lint

--- api/repositories/project.ts
+++ api/repositories/project.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-//import { v4 as uuidv4 } from 'uuid'
+// import { v4 as uuidv4 } from 'uuid'
 import { Role } from '../entities/member'
 import { ProjectRepositoryInterface } from '../entities/interface'
 import { Project } from '../entities/project'
