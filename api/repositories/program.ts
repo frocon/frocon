8182d46165534f1a304a92d481825f21d37ce4af
FlexiblePrintedCircuits <yugehayata.dev@gmail.com>
2021-09-25 23:26:34 +0900

    fix: コメントアウトを削除

--- api/repositories/program.ts
+++ api/repositories/program.ts
@@ -1,8 +1,6 @@
 import { PrismaClient } from '@prisma/client'
-// import { v4 as uuidv4 } from 'uuid'
 import { ProgramRepositoryInterface } from '../entities/interface'
 import { Program } from '../entities/program'
-// import { readSource, writeSource } from '../infrastructures/program'
 
 const prisma = new PrismaClient({ rejectOnNotFound: true })
 
