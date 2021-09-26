d8b0ad41d57626586920330cecd94c6f09438da9
lockdef <locktki@icloud.com>
2021-09-26 10:20:15 +0900

    feat: signupを考慮した遷移ガード

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,12 @@
 import { Context, Middleware } from '@nuxt/types'
 
 const authMiddleware: Middleware = ({ store, route, redirect }: Context) => {
-  if (!store.getters.isLoggedIn) return redirect('/login')
+  if (
+    !store.getters.isLoggedIn &&
+    route.path !== 'signup' &&
+    route.path !== 'login'
+  )
+    return redirect('/login')
   if (route.path === '/login') return redirect('/')
 }
 
