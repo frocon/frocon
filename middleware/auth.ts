c2a9006040dcd1f0476a909f1fe911c7879dcd71
lockdef <locktki@icloud.com>
2021-09-26 10:20:41 +0900

    feat: ログイン時のsignupへのアクセスを阻止

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -3,11 +3,11 @@ import { Context, Middleware } from '@nuxt/types'
 const authMiddleware: Middleware = ({ store, route, redirect }: Context) => {
   if (
     !store.getters.isLoggedIn &&
-    route.path !== 'signup' &&
-    route.path !== 'login'
+    route.path !== '/signup' &&
+    route.path !== '/login'
   )
     return redirect('/login')
-  if (route.path === '/login') return redirect('/')
+  if (route.path === '/login' || route.path === '/signup') return redirect('/')
 }
 
 export default authMiddleware
