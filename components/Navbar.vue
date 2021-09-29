3e8651231257c549e30f0878082a72f6c6e74077
lockdef <locktki@icloud.com>
2021-09-29 23:22:03 +0900

    feat: サインアウトボタンを追加

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -19,24 +19,19 @@
       >
     </div>
     <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        fill="white"
-        class="bi bi-three-dots"
-        viewBox="0 0 16 16"
-      >
-        <path
-          d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"
-        />
-      </svg>
+      <Button @click.native="signOut">ログアウト</Button>
     </div>
   </nav>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   name: 'Navbar',
+  methods: {
+    async signOut() {
+      await this.$fire.auth.signOut()
+      location.reload()
+    },
+  },
 }
 </script>
